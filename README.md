640b930a4356e3c15935234feb593669dddb00f9
BOOK-H4L560D61R\Nicholas <ktspsnicholas@gmail.com>
2023-08-09 22:13:56 +0800

    Create the base logic, Save Data in txt, Future Planning

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 # TrackUsageApp
 
 ## Idea
-  The idea is originated that I want to number of hours or programming, as a way to track my productivity. Github doesn't provide this service as it only tracks the number of commits you have. So I wanted to create a program to do so. 
-  This idea can be extended to tracking all apps that I am using.
+
+The idea is originated that I want to number of hours or programming, as a way to track my productivity. Github doesn't provide this service as it only tracks the number of commits you have. So I wanted to create a program to do so.
+This idea can be extended to tracking all apps that I am using.
 
 ## Planning
-First Step:
-  FrontEnd: Command Line 
-  BackEnd: Tracking App - Python
-  Database: (currently) Excel
 
+First Step:
+FrontEnd: Command Line
+BackEnd: Tracking App - Python
+Database: txt
+
+Important Resources:
+
+- https://dev.to/tkkhhaarree/track-windows-app-usage-time-using-python-h9h
--- main.py
+++ main.py
@@ -3,14 +3,32 @@ from win32gui import GetForegroundWindow
 import psutil
 import time
 import win32process
+import atexit
+import ast
 
-process_time={}
+def exit_handler():
+    with open('record.txt', 'w') as f:
+        f.write(f"{process_time}")
+    
+def open_handler():
+    with open('record.txt') as f:
+        return f.readline()
+
+records = open_handler()
+process_time = ast.literal_eval(records)
+
 timestamp = {}
+atexit.register(exit_handler) #handle this function when exit program
 while True:
+    #get app
     current_app = psutil.Process(win32process.GetWindowThreadProcessId(GetForegroundWindow())[1]).name().replace(".exe", "")
-    timestamp[current_app] = int(time.time())
+    
+    #current time express in seconds
+    timestamp[current_app] = int(time.time()) 
     time.sleep(1)
+    
     if current_app not in process_time.keys():
-        process_time[current_app] = 0
+        process_time[current_app] = 0 #starting time
     process_time[current_app] = process_time[current_app]+int(time.time())-timestamp[current_app]
     print(process_time)
+
