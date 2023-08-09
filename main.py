3c513f9864e04f1e19dc9db16ed6479ed617238b
BOOK-H4L560D61R\Nicholas <ktspsnicholas@gmail.com>
2023-08-09 21:00:06 +0800

    First commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import win32gui
+from win32gui import GetForegroundWindow
+import psutil
+import time
+import win32process
+
+process_time={}
+timestamp = {}
+while True:
+    current_app = psutil.Process(win32process.GetWindowThreadProcessId(GetForegroundWindow())[1]).name().replace(".exe", "")
+    timestamp[current_app] = int(time.time())
+    time.sleep(1)
+    if current_app not in process_time.keys():
+        process_time[current_app] = 0
+    process_time[current_app] = process_time[current_app]+int(time.time())-timestamp[current_app]
+    print(process_time)
